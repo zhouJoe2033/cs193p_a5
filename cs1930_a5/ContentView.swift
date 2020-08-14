0b5b7db4c0a3ddaf9b952009ca847d3f0e275674
zhouJoe2033 <49823919+zhouJoe2033@users.noreply.github.com>
2020-08-14 11:08:02 -0400

    Initial Commit

---
+++ cs1930_a5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cs1930_a5
+//
+//  Created by Joe on 2020-08-14.
+//  Copyright © 2020 Joe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
